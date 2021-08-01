4f0558f65a1f29f4c7670ba837e5a9a75e0ff126
Damola Olusakin <olusakinadedamola@gmail.com>
2021-08-01 23:57:13 +0100

    style bottom navigation and create header

--- constants/icons.js
+++ constants/icons.js
@@ -2,11 +2,15 @@ export const cutlery2 = require("../assets/icons/cutlery2.png");
 export const search8 = require("../assets/icons/search8.png");
 export const like2 = require("../assets/icons/like2.png");
 export const user = require("../assets/icons/user.png");
+export const nearby = require("../assets/icons/nearby.jpeg");
+export const basket = require("../assets/icons/basket.png");
 
 export default {
   cutlery2,
   search8,
   like2,
   user,
+  nearby,
+  basket
   
 }
--- navigation/tabs.js
+++ navigation/tabs.js
@@ -3,15 +3,103 @@ import { View, Image, TouchableOpacity } from 'react-native'
 import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs'
 import { Home } from '../screens'
 import { COLORS, icons } from '../constants'
+import Svg, {Path} from 'react-native-svg'
+import {isIphoneX, isPhonex} from 'react-native-iphone-x-helper'
 
 const Tab = createBottomTabNavigator();
+
+const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
+  var isSelected = accessibilityState.selected
+
+  if(isSelected) {
+    return (
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <View style={{ flexDirection: 'row', position: 'absolute', top: 0 }}>
+            <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+            <Svg
+              width={75}
+              height={61}
+              viewBox="0 0 75 61"
+              >
+                <Path
+                d="M75.2 0v61H0v0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+                fill={COLORS.white}
+                />
+              </Svg>
+              <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+          </View>
+          <TouchableOpacity
+            style={{
+              top: -22.5,
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+              backgroundColor: COLORS.white
+            }}
+            onPress={onPress}
+            >
+              {children}
+            </TouchableOpacity>
+        </View>
+    )
+  } else {
+    return (
+        <TouchableOpacity
+        style={{
+          flex: 1,
+          height: 60,
+          backgroundColor: COLORS.white
+        }}
+        activeOpacity={1}
+        onPress={onPress}
+        >
+          {children}
+        </TouchableOpacity>
+    )
+  }
+}
+
+const CustomTabBar = (props) => {
+  if (isIphoneX()) {
+  return (
+    <View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          height: 30, 
+          backgroundColor: COLORS.white,
+        }}
+      >
+
+      </View>
+    <BottomTabBar {...props.props} />
+    </View>
+  )
+} else {
+  <BottomTabBar {...props.props} />
+}
+}
 
 const Tabs = () => {
   return (
     <Tab.Navigator
       tabBarOptions={{
-        showLabel: false
+        showLabel: false,
+        style: {
+          borderTopWidth: 0,
+          backgroundColor: "transparent",
+          elevation: 0
+        }
       }}
+      tabBar={(props) => (
+        <CustomTabBar
+          props={props}
+          />
+      )}
     >
       <Tab.Screen
           name="Home"
@@ -27,6 +115,11 @@ const Tabs = () => {
                   tintColor: focused ? COLORS.primary : COLORS.secondary
                 }}
                 />
+            ),
+            tabBarButton: (props) => (
+              <TabBarCustomButton
+              {...props}
+              />
             )
           }}
       />
@@ -45,6 +138,11 @@ const Tabs = () => {
                   tintColor: focused ? COLORS.primary : COLORS.secondary
                 }}
                 />
+            ),
+            tabBarButton: (props) => (
+              <TabBarCustomButton
+              {...props}
+              />
             )
           }}
       />
@@ -63,6 +161,11 @@ const Tabs = () => {
                   tintColor: focused ? COLORS.primary : COLORS.secondary
                 }}
                 />
+            ),
+            tabBarButton: (props) => (
+              <TabBarCustomButton
+              {...props}
+              />
             )
           }}
       />
@@ -81,6 +184,11 @@ const Tabs = () => {
                   tintColor: focused ? COLORS.primary : COLORS.secondary
                 }}
                 />
+            ),
+            tabBarButton: (props) => (
+              <TabBarCustomButton
+              {...props}
+              />
             )
           }}
       />
